fdbe459e4ae5e42b47348d462406df7b8f1ab186
Auxiliar TIC INR <auxiliar.tic@PC-AUX-TIC.INR4>
2023-06-30 17:00:18 -0500

    semaforo y funciones

--- IntroJS/5.Funciones/js/index.js
+++ IntroJS/5.Funciones/js/index.js
@@ -1,7 +1,23 @@
 
 
-const prueba=()=>{
-   //document.write("<img src='../src/img/luz-roja.png'/>")
-   document.writeln("<p>¡Hola mundo!</p>");
+const luzRoja=()=>{
+   document.write("<img src='../src/img/luz-roja.png' style='width:200px ;'/>")
+   //document.write("<button onclick={luzAmarilla()}>Clic para luz amarilla</button>")
+   setTimeout(luzAmarilla(),10)
+  
+
+}
+const luzAmarilla=()=>{
+   document.write("<img src='../src/img/luz-amarilla.png' style='width:200px ;'/>")
+  setInterval(luzVerde(),5000)
+  
+   // document.write("<button onclick={luzVerde()}>Clic para luz verde</button>")
+}
+
+const luzVerde=()=>{
+   document.write("<img src='../src/img/luz-verde.png'style='width:200px ;'/>")
+   setInterval(luzRoja(),5000)
+  
+   // document.write("<button onclick={luzRoja()}>Clic para luz roja</button>")
 
 }
