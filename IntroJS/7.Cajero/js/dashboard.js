cc824882084e836b14aa02300e55d0f6943cb791
Santiago Paredes <111004266+Spsantiago@users.noreply.github.com>
2023-07-17 16:31:05 -0500

    actualizacion de dashboard

---
+++ IntroJS/7.Cajero/js/dashboard.js
@@ -0,0 +1,12 @@
+const cuentas = [
+    { nombre: 'Mali', saldo: 200 },
+    { nombre: 'Gera', saldo: 290 },
+    { nombre: 'Maui', saldo: 67 },
+  ];
+  
+
+const usuario = localStorage.getItem('nombre')
+
+const consultarSaldo=()=>{
+    
+}
